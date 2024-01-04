5b74f5fec285f931ffb99c6a0cfc1961457590ec
Dexter Crago <jodieanddexter@gmail.com>
2024-01-04 12:18:18 -0700

    Write algorithim for gameController

---
+++ javascript.js
@@ -0,0 +1,37 @@
+// Start game
+    // let activePlayer;
+
+// Change player
+    // If activePlayer === playerOne
+        // activePlayer = playerTwo;
+    // Else activePlayer = playerOne
+
+// Start turn
+    // activePlayer attempts to place symbol in a location
+    // Check if location already has symbol in it
+        // If empty,
+            // Place symbol in location
+            // Check board
+            // Change player
+        // If occupied, 
+            // Inform player to choose a different location
+    // Start turn
+
+// Check board
+    // Check for 3-in-a-row
+        // If game is not ended,
+            // Check for full board
+
+// Check for 3-in-a-row
+    // If one if found,
+        // End game
+
+// Check for full board
+    // If board is full,
+        // End game
+
+// End game
+    // If 3-in-a-row
+        // Display winner
+    // If tie
+        // Declare tie
