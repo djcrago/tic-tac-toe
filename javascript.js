b798e081d99de69138afda072bb23761cdfb106a
Dexter Crago <jodieanddexter@gmail.com>
2024-01-04 13:44:01 -0700

    Create a 3x3 grid of empty arrays and display it
    This serves as the basis for the board to be updated in the future with
    the players' symbols when they select a position.

--- javascript.js
+++ javascript.js
@@ -6,6 +6,22 @@
     // Include here logic for how to push symbols into a cell
     // Be able to print board with cell values in console
 
+function gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for(let i = 0; i < rows; i++) {
+        board[i] = [];
+        for(let j = 0; j < columns; j++) {
+            board[i].push(' ');
+        }
+    }
+
+    console.log(board);
+}
+
+gameboard();
 
 
 // Game controller
